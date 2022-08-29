b43129e3ea9918d54efda596b476e0fca5aef4cf
toufiqulislam525 <toufiqulislam525@gmail.com>
2022-08-29 12:39:31 +0600

    Api POST adduser created

--- Codes/Apis.py
+++ Codes/Apis.py
@@ -37,3 +37,55 @@ def get_user_by_id(userid: int):
         return user
     else:
         raise HTTPException(status_code=404, detail="Id not in database")
+
+
+# API : POST adduser
+# Input Format for Post method
+class User(BaseModel):
+    m_id: int  # Id of the person who requested post method
+    id: int
+    name: str
+    role: str
+
+
+# API : POST adduser
+@app.post("/adduser")
+def post_add_user(user: User):
+    m_id = user.m_id
+    id = user.id
+    name = user.name
+    role = user.role
+
+    if (role != 'employee'):
+        raise HTTPException(
+            status_code=404, detail="Role of the added person is not Employee")
+
+    sql = "Select * from users where id=?"
+    values = (m_id,)
+    user = db_query(sql, values)
+    user = r_d(user)
+    if user:
+        # Check If requested user is manager
+        if (user[0]['role'] != 'manager'):
+            raise HTTPException(
+                status_code=404, detail="Requested id is not a manager")
+
+    else:
+        raise HTTPException(
+            status_code=404, detail="Id of Requested Person is not in Database")
+
+    values = (id,)
+    # Check if added id is already in the database
+    existance_check = db_query(sql, values)  # Previous sql
+
+    if (existance_check):
+        raise HTTPException(
+            status_code=404, detail="Id of Employee already Exists")
+
+    else:
+        # Add Employee
+        sql = "Insert into users values(?,?,?)"
+        values = (id, name, role)
+        db_query(sql, values)
+        # Return All employees with the new employee added
+        return get_users()
