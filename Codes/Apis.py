ad9b31ad583da3283ee5c9ab9f46df57faf07d0f
toufiqulislam525 <toufiqulislam525@gmail.com>
2022-08-29 11:52:11 +0600

    GET users Api created

--- Codes/Apis.py
+++ Codes/Apis.py
@@ -3,4 +3,18 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from datetime import datetime
 
+# Custom Functions
+from Database_Handler import db_query
+from Custom_Functions import response_dictionary as r_d
+
+
 app = FastAPI()
+
+
+@app.get("/users")
+def get_users():
+    sql = "Select * from users"
+    values = False
+    users = db_query(sql, values)
+    users = r_d(users)
+    return users
