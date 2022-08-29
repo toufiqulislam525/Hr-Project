e975286394c7fc6066ce771502debfd19f47ecc9
toufiqulislam525 <toufiqulislam525@gmail.com>
2022-08-29 12:50:30 +0600

    Updated custom functions

--- Codes/Custom_Functions.py
+++ Codes/Custom_Functions.py
@@ -7,3 +7,13 @@ def response_dictionary(data):
         dict['role'] = i[2]
         l.append(dict)
     return l
+
+
+def response_dictionary_2(data):
+    l = []
+    for i in data:
+        dict = {}
+        dict['id'] = i[0]
+        dict['time'] = i[1]
+        l.append(dict)
+    return l
